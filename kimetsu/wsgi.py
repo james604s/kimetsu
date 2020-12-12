437f6350fbb31e9f8904c16465c90bf1dcf5507c
Squid Tseng <47516926+james604s@users.noreply.github.com>
2020-12-12 14:31:46 +0800

    update heroku

--- kimetsu/wsgi.py
+++ kimetsu/wsgi.py
@@ -7,15 +7,15 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
-# import os
+import os
 
-# from django.core.wsgi import get_wsgi_application
-
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kimetsu.settings')
-
-# application = get_wsgi_application()
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kimetsu.settings')
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+# from django.core.wsgi import get_wsgi_application
+# from whitenoise.django import DjangoWhiteNoise
+
+# application = get_wsgi_application()
+# application = DjangoWhiteNoise(application)
