27eb56b594195114bb98877b3ebb7044a97830c9
Squid Tseng <47516926+james604s@users.noreply.github.com>
2020-12-24 16:51:10 +0800

    login update

--- myauth/views.py
+++ myauth/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from django.views.generic import View
 from django.contrib.auth.models import User
-
-# Create your views here.
+from django.contrib.auth import authenticate
+from myauth.myauth import login_form
+class MyLoginView(View):
+    template_name = 'myauth/login.html'
+    def post(self, request):
+        user = authenticate(request, username = request.POST['username'], password=request.POST['password'])
+        if user is not None:
+            login(request, user)
+            return HttpResponse("Login Successful")
+        return HttpResponse("Login Failed")
